b5abd4049ea47423c2c0cdb88b1126dc464cc7d4
apilatosba <aposdapulos098@outlook.com>
2024-01-18 22:15:30 +0300

    got the libraries. ready to go

--- Noita Sand Simulation/Program.cs
+++ Noita Sand Simulation/Program.cs
@@ -1,6 +1,19 @@
-﻿namespace Noita_Sand_Simulation {
+﻿using Raylib_cs;
+
+namespace Noita_Sand_Simulation {
    class Program {
       static void Main(string[] args) {
+         Raylib.InitWindow(800, 600, "Noita Sand Simulation");
+         Raylib.SetTargetFPS(144);
+
+         while (!Raylib.WindowShouldClose()) {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+
+            Raylib.DrawText("Noita Sand Simulation", 10, 10, 20, Color.WHITE);
+
+            Raylib.EndDrawing();
+         }
       }
    }
 }
